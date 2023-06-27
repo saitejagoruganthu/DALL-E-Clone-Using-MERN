37f961afaa0bedb7ba7742b2ac7d085c0402505f
Tirumala Saiteja Goruganthu <86722185+saitejagoruganthu@users.noreply.github.com>
2023-06-27 14:57:38 -0700

    Update index.js
    Updated CORS origin

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+      origin: 'https://dalle.saitejagoruganthu.com/'
+    }
+));
 app.use(express.json({limit: '50mb'}));
 
 app.use('/api/v1/post', postRoutes);
