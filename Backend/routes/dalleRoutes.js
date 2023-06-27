9aa239b89034a7767114880678e5c95e736ea0e1
Tirumala Saiteja Goruganthu <86722185+saitejagoruganthu@users.noreply.github.com>
2023-06-27 15:20:15 -0700

    Update dalleRoutes.js

--- Backend/routes/dalleRoutes.js
+++ Backend/routes/dalleRoutes.js
@@ -28,7 +28,6 @@ router.route('/').post(async (req,res)=>{
         })
 
         const image = aiResponse.data.data[0].b64_json;
-        res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200).json({photo: image});
     }
     catch(err)
