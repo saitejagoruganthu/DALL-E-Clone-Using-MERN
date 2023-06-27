d7fc1b15fe7505de2ae7e5bd93c2de8a3c431587
Tirumala Saiteja Goruganthu <86722185+saitejagoruganthu@users.noreply.github.com>
2023-06-27 14:28:36 -0700

    Update dalleRoutes.js
    Added CORS origin

--- Backend/routes/dalleRoutes.js
+++ Backend/routes/dalleRoutes.js
@@ -28,6 +28,7 @@ router.route('/').post(async (req,res)=>{
         })
 
         const image = aiResponse.data.data[0].b64_json;
+        res.set('Access-Control-Allow-Origin', 'https://dalle.saitejagoruganthu.com/');
         res.status(200).json({photo: image});
     }
     catch(err)
